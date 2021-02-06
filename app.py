c4ed64bf60e4143138ad5487c2c631cf78df10bc
Sabyasachi Sharma <sabyasachi512@gmail.com>
2021-02-06 05:30:05 +0530

    summarizer added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import io
 import os
 import sys
 from bs4 import BeautifulSoup
+from summarize_url import SummarizeNp
 
 app = Flask(__name__, template_folder='./frontend/templates', static_folder='./frontend/static')
 app.secret_key = 'bhaisa'
@@ -20,6 +21,7 @@ def hello_world():
         if request.form['action'] == 'pdf':
             return redirect(url_for("get_pdf"))
         else:
+            # return redirect(url_for("get_summary"))
             return '<h1> UNDER CONSTRUCTION</h1>'
     else:
         return render_template('index.html')
@@ -48,19 +50,22 @@ def get_pdf():
                          attachment_filename=f'{title_txt}.pdf')
 
 
-# @app.route("/get_summary", methods=['POST', 'GET'])
-# def get_summary():
-#     url = session['get_pdf']['url']
-#     method_object = methods_file.GetResourceMethods()
-#     parsed_uri = urlparse(url)
-#     result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-#     html_str, title_txt = method_object.select_parser(input_url_host_only=result, url_full=url)
-#     soup = BeautifulSoup(html_str, 'lxml')
-#     text = soup.findAll('p')
-#     return_html = ''
-#     for item in text:
-#         return_html += item.text
-#     return return_html
+@app.route("/get_summary", methods=['POST', 'GET'])
+def get_summary():
+    url = session['get_pdf']['url']
+    method_object = methods_file.GetResourceMethods()
+    parsed_uri = urlparse(url)
+    result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    html_str, title_txt = method_object.select_parser(input_url_host_only=result, url_full=url)
+    soup = BeautifulSoup(html_str, 'lxml')
+    text = soup.findAll('p')
+    return_html = ''
+    for item in text:
+        return_html += item.text
+    summarizer = SummarizeNp()
+    summary = summarizer.generate_summary(html_str=return_html)
+    print('came here', file=sys.stdout)
+    return summary
 
 def make_html_pdf(html_str):
     """Download the currently displayed page to target_path."""
@@ -90,4 +95,6 @@ def make_html_pdf(html_str):
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run(host='127.0.0.1', port=1200)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
